37e4afd1d9ca074fba4c348f36aac39832e5020b
PAL-1 <Soham_Basu@Dell.com>
2019-08-07 14:05:09 +0530

    Assign 6.1

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,9 +1,10 @@
 package io.pivotal.pal.tracker;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -14,11 +15,8 @@ public class PalTrackerApplication {
 
 
     @Bean
-    public JdbcTimeEntryRepository timeEntryRepository()
+    public JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource)
     {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
-        //return new InMemoryTimeEntryRepository();
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
